4535ca07a1f088a4e6e07ee10d6421ca19824eaf
dantedxwolf <teacher.martin@gmail.com>
2021-01-11 12:13:07 +0600

    LandingPage updated

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,6 +3,7 @@ import { withRouter, RouteComponentProps } from "react-router-dom";
 import {
   Paper, Typography, Button, TextField
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { LandingPageElement } from "../elements/LandingPageElement";
 import { addNameAction } from "../actions/NameActions";
 import { connect } from "react-redux";
@@ -11,8 +12,21 @@ export interface LandingProps extends RouteComponentProps{
   addNameAction: Function;
 }
 
+const useStyles = makeStyles({
+  eachRow: {
+    margin: "10px"
+  },
+  LandingPaper: {
+    padding: "10px",
+    margin: "auto",
+    textAlign: "center",
+    height:"40vh"
+  }
+});
 
-const _Landing: React.FunctionComponent<LandingProps> = ({ history,addNameAction }) => {
+
+const _Landing: React.FunctionComponent<LandingProps> = ({ history, addNameAction }) => {
+  const classes = useStyles();
   const [userName, setUserName] = React.useState("");
   const handleClick = (e: React.MouseEvent<HTMLElement>) => {
     if (userName.length === 0) {
@@ -24,12 +38,12 @@ const _Landing: React.FunctionComponent<LandingProps> = ({ history,addNameAction
   }
   return (
     <LandingPageElement>
-      <Paper elevation={10} style={{padding:"10px",height:"400px"}}>
-        <Typography variant="h4" color="initial">Welcome to ShowwCase!</Typography>
-        <Typography variant="h6" color="initial">Enter your name to get started</Typography>
-        <TextField onChange={e => setUserName(e.target.value)} required variant="outlined" type="text" label="Enter Your Name" />
+      <Paper elevation={10} className={classes.LandingPaper}>
+        <Typography className={ classes.eachRow} variant="h4" color="initial">Welcome to ShowwCase!</Typography>
+        <Typography className={ classes.eachRow} variant="h6" color="initial">Enter your name to get started</Typography>
+        <TextField  className={ classes.eachRow} onChange={e => setUserName(e.target.value)} required variant="outlined" type="text" label="Enter Your Name" />
         <br/>
-        <Button onClick={e => handleClick(e)} variant="contained" color="secondary">Enter</Button>
+        <Button className={ classes.eachRow} onClick={e => handleClick(e)} variant="contained" color="secondary">Enter</Button>
       </Paper>
     </LandingPageElement>
   )
