57d460018183c8382fc9ecb6cdcfffb4054986d4
dantedxwolf <teacher.martin@gmail.com>
2021-01-11 15:50:51 +0600

    Modal Basic added

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
-import {NameReducer } from "./NameReducer";
+import { NameReducer } from "./NameReducer";
+import { NameInterface } from "./NameReducer";
+
+
 
 export interface StoreState{
-  name: string;
+  NameReducer: NameInterface;
 }
 
 
 // we have to put the store state generic here
-export default combineReducers({
+export default combineReducers<StoreState>({
     NameReducer
 });
